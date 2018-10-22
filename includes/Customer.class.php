fe92223969f9f4262df83e7308f6ddf04be87852
Waalax <alexandre.joyeux@ecl15.ec-lyon.fr>
2018-10-22 15:17:40 +0200

    Finalisation de l'objet Client

--- includes/Customer.class.php
+++ includes/Customer.class.php
@@ -8,11 +8,103 @@
 class Customer
 {
 
-    private $number;
+    private $idCustomer;
 
     private $name;
 
     private $surname;
 
-    private $adress;
+    private $city;
+
+    /**
+     * Méthode publique de construction de la classe Client (Customer).
+     *
+     * @param int $idCustomer
+     */
+    public function __construct($idCustomer)
+    {
+        $this->idCustomer = $idCustomer;
+    }
+
+    // Fonctions de modification de l'objet Client (Customer).
+    
+    /**
+     * Méthode publique modifiant l'id du Client (Customer).
+     *
+     * @param int $idCustomer
+     */
+    public function setIdCustomer($idCustomer)
+    {
+        $this->idCustomer = $idCustomer;
+    }
+
+    /**
+     * Methode publique modifiant le nom du Client (Customer).
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Méthode publique modifiant le prénom du Client (Customer).
+     *
+     * @param string $surname
+     */
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+    }
+
+    /**
+     * Méthode publique modifiant la commune du Client (Commune).
+     *
+     * @param string $city
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+    }
+
+    /**
+     * Méthode publique donnant accès à l'id du Client (Customer).
+     *
+     * @return int
+     */
+    public function getIdCustomer()
+    {
+        return $this->idCustomer;
+    }
+
+    /**
+     * Méthode publique donnant accès au nom du Client (Customer).
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Méthode publique donnant accès au prénom du Client (Customer).
+     *
+     * @return string
+     */
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+
+    /**
+     * Méthode publique donnant accès à la commune du Client (Customer).
+     *
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
 }
